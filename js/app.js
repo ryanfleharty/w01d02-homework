07311d2f991fd2869c07ce96904560c0ea4f631d
Destiny <dshillis1@gmail.com>
2019-03-19 22:28:19 -0600

    Working on homework

---
+++ js/app.js
@@ -0,0 +1,41 @@
+//Part 1: Terms
+// 1. The difference between interpolation and concatenation is that concatenation is used to combine to strings 
+// and interpolation allows you to insert values of variables into strings.
+
+// 2. DRY stands for "Don't Repeat Yourself," and it's important because it helps us clean up our code so that it
+// runs faster/smoother and is easier for other programmers to read and edit. The tools we have learned to create
+// DRY code are while and for loops.
+
+// 3. The difference between declaring a variable and assigning a value to a variable is when you declare a 
+// variable, you are saying that you are going to use it to hold a value (ex. let dogName). Assigning a value is 
+// when you actually give the variable a value (ex. dogName = "Dubs").
+
+// 4. We should use let when we want to be able to redefine a variable. We should use const when we want a variable 
+// to stay constant, or unable to be redefined.
+
+// 5. A parent directory is a folder with contents within it. 
+
+// 6. To find out how to use a Unix command without the internet, you can use the man + the command to pull up the 
+// manual page for that command. 
+
+// 7. Tab completion is the bash and text editors' ability to predict what you are trying to write after you type 
+// a few characters and hit tab. It speeds things up a bit, but mostly helps prevent spelling errors.
+
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+//console.log(a < b);
+//console.log(c > d);
+//console.log('Name' === 'Name');
+//console.log(a < b < c);
+//console.log(a < a < d);
+//console.log(e === 'Kevin');
+//console.log(48 !== '48');
+
+
+
+//Part 3: While loops
