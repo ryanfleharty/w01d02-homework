627ab8b5c1f0687b693cbe4183c6e8dda0d7f303
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-03-19 20:42:47 -0600

    completed parts 1-3

---
+++ app.js
@@ -0,0 +1,59 @@
+// 1. Interpolation is inserting values of variables into
+// strings to use for console logs.by using `` and ${}.
+// const hello = "hello world!";
+//
+// console.log(`I am thrilled to be around here ${hello}`);
+//
+//  While Concatenation
+// is using strings together with variables.
+// const firstName = "Ben";
+// const lastName = "wall";
+// console.log("Welcome back " + firstName + lastName);
+
+
+// 2. DRY stands for don't repeat yourself. which is mainly
+// to have optimized and clean code so other coders will be
+// able to read and understand what you're trying to do.
+// Instead of using a While loops for every function. There is
+// the For loops which can cut down on code to run faster.
+
+
+// 3. Defining a variable is declaring it for the first time.
+// these will be defined with an initial value to that variable.
+// which can be a string or a number.
+
+// 4. A const is used when the value of the variable will not change.
+// While it can be modified using that const's name with .push.
+// Let is another way of declaring a variable but it's value can be changed
+// when it is mentioned throughout the code.
+
+// 5. A parent directory is the directory or folder that hosts other files within its path.
+// that can consist of the main project folder being the parent directory
+// of the index, css and javascript files for creating a website.
+
+// 6. Within the terminal you can use the command "man" before the command's name
+// to have a manual description of what the possibilities of the command holds, along
+// with its purpose.
+
+// 7. Tab completion is used to help the user within the terminal to auto fill
+// the rest of the object. It streamlines the workflow for going through directories.
+// Along with skipping over spelling mistakes that could happen if it was typed.
+
+
+// 1. a < b
+// 2. c > d
+// 3. 'name' = 'name'
+// 4. a < b < c
+// 5. a = a < d
+// 6. e = 'Kevin'
+// 7. 48 = '48'
+
+
+1. It's not an infinite loop because there is nothing to check for the loop to repeat.
+
+
+2. it is  an infinite loop because it only loops while runProgram remains true.
+But within the function it will return false but that is within the function staying locally
+in the function and will not change the initial value of runProgram keeping it in a loop.
+
+3. 
