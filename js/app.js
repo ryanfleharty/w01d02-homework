b619b4c51ad827543188b717d085bded8766a151
Jake Shelton <jakeshelton@Archies-Howl.local>
2019-03-19 18:57:33 -0600

    hw2 thru part2

---
+++ js/app.js
@@ -0,0 +1,24 @@
+console.log('Up and running');
+
+//1. What is the difference between interpolation and concatenation? Give an example of each.
+//interpolation is when a string is inserted into an existing string via ${ref}
+//concatenation is appending strings together and converting into strings.
+
+//2. What does the acronym DRY stand for? Why should we pay attention to it? What programming tools have we learned to write DRY code?
+//DRY stands for "Don't Repeat Yourself".
+//Following DRY standards will result in easier to read, execute, and maintain code.
+//loops (and functions) help us to write DRY code by allowing us to call the same code again in multiple places
+
+//3. What is the difference between declaring a variable and assigning a value to a variable? What do we mean when we say "define" a variable?
+
+//4. When should we use const and when should we use let?
+
+//5. Unix question: What is a "parent" directory?
+
+//6. Unix, again: If you're not sure about how to use a Unix command, how can you get more info without being connected to the internet?
+//Use the "man" command to access the manual in the terminal or just type the command without any parameters or -flags
+
+//7. More Unix: What is "tab completion" and why is it awesome?
+//Its when you start typing a word in the terminal (or in your text editor) and hit tab and it auto-suggests a correct "completion" of that word to a variable or function call or directory name, etc.
+
+
